6709253fd796c19189569b0141199f59cf41f64d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2020-03-17 10:10:46 +0100

    do also "obj" arcs

--- symbolic-fixer/Fixer.hs
+++ symbolic-fixer/Fixer.hs
@@ -6,8 +6,9 @@ import DepLib
 import qualified Data.ByteString.Lazy.Char8 as L
 
 data Arc = Arc {arcSource, arcTarget :: Int, arcLabel :: L.ByteString }
-isSubject :: Entry -> Bool
-isSubject Entry{..} = entryLabel == "nsubj"
+
+-- isSubject :: Entry -> Bool
+-- isSubject Entry{..} = entryLabel == "nsubj"
 
 kidsOf :: Int -> Sentence -> [(Int,Entry)]
 kidsOf parent es = [(i,e) | (i,e@Entry{..}) <- zip numbering es, entryParent == parent]
@@ -20,8 +21,8 @@ conjFixer1 es = concat $ zipWith enhancer numbering es
   where enhancer eIndex e
           = [Arc {arcSource = kidIndex,
                   arcTarget = entryParent e,
-                  arcLabel = "nsubj"}
-            | isSubject e,
+                  arcLabel = entryLabel e}
+            | entryLabel e `elem` ["nsubj","obj"],
               (kidIndex,kid) <- kidsOf eIndex es,
               entryLabel kid == "conj"]
 
