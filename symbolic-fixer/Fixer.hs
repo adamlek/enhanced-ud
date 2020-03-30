41b3612ba92a035cefce30a7dfa583db99218687
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2020-03-30 10:31:39 +0200

    cleanup

--- symbolic-fixer/Fixer.hs
+++ symbolic-fixer/Fixer.hs
@@ -28,28 +28,17 @@ entryToArc i e = Arc {arcSource = entryParent e, arcTarget = i, arcLabel = entry
 
 -- See https://universaldependencies.org/u/overview/enhanced-syntax.html for documentation
 
--- type 1
--- tags: nsubj, obj, amod
--- Meet -----> Paul -----> Mary
---      nsubj        conj
-conjEnhancer :: Sentence -> [Arc]
-conjEnhancer es = concat $ zipWith enhancer numbering es
-  where enhancer eIndex e
-          = [Arc {arcTarget = kidIndex,
-                  arcSource = entryParent e,
-                  arcLabel = entryLabel e}
-            | entryLabel e `elem` ["nsubj","obj", "amod"],
-              (kidIndex,kid) <- kidsOf eIndex es,
-              entryLabel kid == "conj"]
-
 
 -- | Examples 5->6 and and 7->8.
 -- Have:
+-- was <----- reading -----> watching
+--      aux           conj
 -- She <----- Reading -----> Watching
 --      nsubj          conj
 -- To add:
 --     <-------------------
 --             nsubj
+--              aux
 conjEnhancer2 :: Sentence -> [Arc]
 conjEnhancer2 es = concat $ zipWith enhancer numbering es
   where enhancer eIndex e
@@ -63,22 +52,48 @@ conjEnhancer2 es = concat $ zipWith enhancer numbering es
               -- kidIndex ~ watching
               entryLabel kid == entryLabel e]
 
+-- | Examples 11->12; 13->14; 15->16; 17->18; 21->22.
+-- Have:
+-- tags: nsubj, obj, amod
+-- Meet -----> Paul -----> Mary
+--      nsubj        conj
+conjEnhancer :: Sentence -> [Arc]
+conjEnhancer es = concat $ zipWith enhancer numbering es
+  where enhancer eIndex e
+          = [Arc {arcTarget = kidIndex,
+                  arcSource = entryParent e,
+                  arcLabel = entryLabel e}
+            | entryLabel e `elem` ["nsubj","obj", "amod"],
+              (kidIndex,kid) <- kidsOf eIndex es,
+              entryLabel kid == "conj"]
+
+-- | Example 23->24; 25->26
+-- type 2
+-- interior <------ look ------> new
+--           nsubj        xcomp
+xcompEnhancer :: Sentence -> [Arc]
+xcompEnhancer es = concat $ zipWith enhancer numbering es
+  where enhancer eIndex e
+          = [Arc {arcTarget = e2Index,
+                  arcSource = eIndex,
+                  arcLabel = "xsubj"}
+            | entryLabel e `elem` ["xcomp"],
+              (e2Index,e2) <- zip numbering es,
+              entryLabel e2 == "nsubj",
+              entryParent e2 == entryParent e]
+
 -- | Examples 27 -> 30 and 33 -> 34. Not that this rule conflicts with
 -- 31->32. Supposedly the choice should be determined by lexical
 -- semantics.
-
 -- boy ----------> lived -------> who
 --      acl:recl          nsubj
-
 -- TO:
-
 -- boy ----------> lived
 --      acl:recl
 --     <----------
 --        nsubj
 --     --------------------------> who
 --                 ref
-
 relEnhancer :: Sentence -> [Arc]
 relEnhancer es = concat $ zipWith enhancer numbering es
   where enhancer eIndex e = concat
@@ -102,35 +117,7 @@ relativizeLabel l = if l == "advmod"
                     then "obl"
                     else l
 
--- type 2
--- interior <------ look ------> new
---           nsubj        xcomp
-xcompEnhancer :: Sentence -> [Arc]
-xcompEnhancer es = concat $ zipWith enhancer numbering es
-  where enhancer eIndex e
-          = [Arc {arcTarget = e2Index,
-                  arcSource = eIndex,
-                  arcLabel = "xsubj"}
-            | entryLabel e `elem` ["xcomp"],
-              (e2Index,e2) <- zip numbering es,
-              entryLabel e2 == "nsubj",
-              entryParent e2 == entryParent e]
 
--- type 3
--- she <----- reading -----> watching
---      subj          conj
--- was <----- reading -----> watching
---      aux           conj
-conj3Enhancer :: Sentence -> [Arc]
-conj3Enhancer es = concat $ zipWith enhancer numbering es
-  where enhancer eIndex e
-          = [Arc {arcTarget = e2Index,
-                  arcSource = eIndex,
-                  arcLabel = entryLabel e}
-            | entryLabel e `elem` ["nsubj", "aux"],
-              (e2Index,e2) <- zip numbering es,
-              entryLabel e2 == "conj",
-              entryParent e2 == entryParent e]
 
 applyDelete :: [Arc] -> [Arc]
 applyDelete (DeleteOld x:xs) = filter ((/= x) . arcTarget) (applyDelete xs)
@@ -138,7 +125,8 @@ applyDelete (x:xs) = x:applyDelete xs
 applyDelete [] = []
 
 allEnhancer :: Sentence -> [Arc]
-allEnhancer s = applyDelete (relEnhancer s ++ xcompEnhancer s ++ conjEnhancer s ++ conjEnhancer2 s ++ sentenceToArcs s)
+allEnhancer s =
+  applyDelete (relEnhancer s ++ xcompEnhancer s ++ conjEnhancer s ++ conjEnhancer2 s ++ sentenceToArcs s)
 
 type Enhancement = [(Int,L.ByteString)] -- list of parent/head and label.
 
